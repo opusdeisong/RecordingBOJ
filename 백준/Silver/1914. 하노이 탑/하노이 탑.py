aaa23a6f2e601dfd58bd76e49d22581f671205c2
opusdeisong <105193807+opusdeisong@users.noreply.github.com>
2022-05-24 14:42:38 +0900

    [Silver II] Title: 하노이 탑, Time: 880 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1914. 하노이 탑/하노이 탑.py
@@ -0,0 +1,18 @@
+def move_disk(disk_num, start_peg, end_peg):
+    print("%d %d" % (start_peg, end_peg))
+
+
+def hanoi(num_disks, start_peg, end_peg):
+    if num_disks == 1:
+        return move_disk(num_disks, start_peg, end_peg)
+
+    mid_peg = (6 - start_peg - end_peg)
+    hanoi(num_disks - 1, start_peg, mid_peg)
+    move_disk(num_disks, start_peg, end_peg)
+    hanoi(num_disks - 1, mid_peg, end_peg)
+
+
+num_disks = int(input())
+print(2 ** num_disks - 1)
+if num_disks <= 20:
+    hanoi(num_disks, 1, 3)
