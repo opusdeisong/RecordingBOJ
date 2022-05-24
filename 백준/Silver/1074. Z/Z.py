1b7b05dfc7752643cbe8db1e873c6a7dad9d98da
opusdeisong <105193807+opusdeisong@users.noreply.github.com>
2022-05-24 14:39:25 +0900

    [Silver I] Title: Z, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1074. Z/Z.py
@@ -0,0 +1,11 @@
+N, r, c = map(int, input().split())
+
+
+def sol(N, r, c):
+    if N == 0:
+        return 0
+    else:
+        return 2 * (r % 2) + (c % 2) + 4 * sol(N - 1, r // 2, c // 2)
+
+
+print(sol(N, r, c))
